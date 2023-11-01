f62333dbab1b9d41997012761282c4245f45424d
SoftYourMinds <maximbarishov06@gmail.com>
2023-11-01 09:42:10 +0200

    set up base architecture

---
+++ src/app/currency-converter/currency-converter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrencyConverterComponent } from './currency-converter.component';
+
+
+
+@NgModule({
+  declarations: [
+    CurrencyConverterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CurrencyConverterModule { }
