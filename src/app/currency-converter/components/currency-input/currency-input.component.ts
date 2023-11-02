2f33c1d80144fff77b541cf1ffd3c668c83a02b3
SoftYourMinds <maximbarishov06@gmail.com>
2023-11-02 22:51:10 +0200

    code refactoring for currency input component

--- src/app/currency-converter/components/currency-input/currency-input.component.ts
+++ src/app/currency-converter/components/currency-input/currency-input.component.ts
@@ -7,45 +7,51 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./currency-input.component.scss']
 })
 export class CurrencyInputComponent implements OnInit{
-  // @Input() valueInput: number = 0;
-  // @Output() valueChanged: EventEmitter<number> = new EventEmitter<number>();
+  @Input() currencyAmountInput: string = "0";
+  @Output() currencyAmountChanged: EventEmitter<string> = new EventEmitter<string>();
+  currencyAmountControl: FormControl = new FormControl(this.currencyAmountInput);
 
-  public currencyAmount: string = '0';
-  public currencyAmountControl:FormControl = new FormControl('0');
-
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
-
+    this.currencyAmountControl.valueChanges.subscribe(inputValue => this.handleInput(inputValue));
   }
 
-  onInput(event: Event): void {
-    const userInputValue = (event.target as HTMLInputElement).value;
-    if (!userInputValue) return;
-
-    const formated = userInputValue.replace(/^0+/g, '0').replace(/[^\d.]/ig, '');
-    const currencyAmount = parseFloat(formated);
-
-    if(/^[0-9,]+\.$/.test(userInputValue)) {
-
-      console.log(formated,/^[0-9]+.$/i.test(formated));
-      return
-    };
-
-    if(isNaN(currencyAmount)) {
-      this.currencyAmountControl.setValue(formated);
-      return
-    };
-
-    this.currencyAmountControl.setValue(
-      currencyAmount.toLocaleString('en-EU', {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-    }));
+  private handleInput(inputValue: string): void {
+    const formattedInput = this.formatInput(inputValue);
+    this.currencyAmountControl.setValue(formattedInput, { emitEvent: false });
   }
 
-  // onValueChanged() {
-  //   this.valueChanged.emit(this.value)
-  // }
+  private formatInput(inputValue: string): string {
+    if (!inputValue) return "0";
+
+    const cleanedInput = this.cleanInput(inputValue);
+    const currencyAmount = parseFloat(cleanedInput);
+
+    if (this.shouldSkipFormatting(inputValue)) return cleanedInput;
+
+    if (isNaN(currencyAmount)) return cleanedInput;
+
+    return this.formatCurrency(currencyAmount);
+  }
+
+  private cleanInput(inputValue: string): string {
+    return inputValue.replace(/[^0-9.]/g, '').replace(/^0+/, '0');
+  }
+
+  private shouldSkipFormatting(inputValue: string): boolean {
+    return /^[0-9,]+\.$/.test(inputValue);
+  }
+
+  private formatCurrency(currencyAmount: number): string {
+    return currencyAmount.toLocaleString('en-EU', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2
+    });
+  }
+
+  public onCurrencyAmountChanged(): void {
+    this.currencyAmountChanged.emit(this.currencyAmountControl.value);
+  }
+
 }
